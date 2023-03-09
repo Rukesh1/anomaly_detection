ef0696b88c02e731164b720ce9539bca0335cdd8
Rukesh <rukesh.sekar@ideas2it.com>
2023-03-09 16:23:30 +0530

    version 2

--- model_training.py
+++ model_training.py
@@ -10,7 +10,7 @@ df=pd.read_excel("/home/ubuntu/Desktop/e5_anamoly_detection/AD_ DataWorkflowMetr
 #df['time_difference_min']=df.apply(lambda x: time_diff_min(x['time_difference']),axis=1)
 df_1=df[['process_name','start_date','end_date','total','success','needs_attention','skipped','failed']]
 random_state = np.random.RandomState(42)
-model=IsolationForest(n_estimators=100,max_samples='auto',contamination=float(0.01),random_state=random_state)
+model=IsolationForest(n_estimators=50,max_samples='auto',contamination=float(0.1),random_state=random_state)
 
 model.fit(df_1[['total','success','needs_attention','failed','skipped']])
 pkl.dump(model, open('anomaly_model.pkl', 'wb'))
