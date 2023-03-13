520ec7bd0fa9a68af5d46ab48fb8c02a56a98921
Rukesh <rukesh.sekar@ideas2it.com>
2023-03-13 12:57:38 +0530

    experiment 2 without skip feature

--- model_training.py
+++ model_training.py
@@ -8,11 +8,11 @@ import pickle as pkl
 df=pd.read_excel("/home/ubuntu/Desktop/e5_anamoly_detection/AD_ DataWorkflowMetrics.xlsx")
 
 #df['time_difference_min']=df.apply(lambda x: time_diff_min(x['time_difference']),axis=1)
-df_1=df[['process_name','start_date','end_date','total','success','needs_attention','skipped','failed']]
+df_1=df[['process_name','start_date','end_date','total','success','needs_attention','failed']]
 random_state = np.random.RandomState(42)
 model=IsolationForest(n_estimators=10,max_samples='auto',contamination=float(0.01),random_state=random_state)
 
-model.fit(df_1[['total','success','needs_attention','failed','skipped']])
+model.fit(df_1[['total','success','needs_attention','failed']])
 pkl.dump(model, open('anomaly_model.pkl', 'wb'))
 print(model)
 
